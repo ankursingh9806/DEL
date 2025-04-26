c09de48c6fe8e0c673205a0b07a8654d9b61707d
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-04-26 15:58:14 +0530

    update

---
+++ LeetCode/A - Two Pointers/1E - 26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+// approach: two pointers
+// time complexity: O(n)
+// space complexity: O(1)
+
+var removeDuplicates = function (nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
