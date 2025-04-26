3ba48b585135ab49714ea1ddcaed7ca7ec668670
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-04-26 15:56:29 +0530

    update

---
+++ LeetCode/A - Bit Manipulation/1E - 136. Single Number.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/single-number/description/
+
+// approach: bit manipulation
+// time complexity: O(n)
+// space complexity: O(1)
+
+var singleNumber = function (nums) {
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        res ^= nums[i];
+    }
+    return res;
+};
