4fba0f2f63e99fa25d9f1627034c3102272ac0ad
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-04-26 16:14:24 +0530

    update

---
+++ LeetCode/DS - Linked List/1E - 83. Remove Duplicates from Sorted List.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+// approach: iterative traversal
+// time complexity: O(n)
+// space complexity: O(1)
+
+var deleteDuplicates = function (head) {
+    let curr = head;
+    while (curr !== null && curr.next !== null) {
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+    return head;
+};
