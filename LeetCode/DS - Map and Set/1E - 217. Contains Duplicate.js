5dadcbbfd55a3172ad7ea60857f28a9d05b1ea6e
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-04-26 16:09:03 +0530

    update

---
+++ LeetCode/DS - Map and Set/1E - 217. Contains Duplicate.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+// approach: brute force
+// time complexity: O(n^2)
+// space complexity: O(1)
+
+var containsDuplicate = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+// approach: set
+// time complexity: O(n)
+// space complexity: O(n)
+
+var containsDuplicate = function (nums) {
+    let set = new Set(nums);
+    return set.size !== nums.length;
+};
