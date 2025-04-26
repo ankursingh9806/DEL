8c4d4ce12fb6ad25cdeb603aff8084bd971a6bb5
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-04-26 16:21:05 +0530

    update

---
+++ LeetCode/DS - Array and String/1E - 58. Length of Last Word.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/length-of-last-word/description/
+
+// approach: brute force
+// time complexity: O(n)
+// space complexity: O(1)
+
+var lengthOfLastWord = function (s) {
+    let count = 0;
+    let i = s.length - 1;
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+    while (i >= 0 && s[i] !== ' ') {
+        count++;
+        i--;
+    }
+    return count;
+};
