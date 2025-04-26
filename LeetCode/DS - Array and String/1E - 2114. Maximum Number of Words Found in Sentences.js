f078263a60a810991faaf2feeab7f8da7cb76150
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-04-26 16:19:39 +0530

    update

---
+++ LeetCode/DS - Array and String/1E - 2114. Maximum Number of Words Found in Sentences.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/description/
+
+// approach: brute force
+// time complexity: O(n * m)
+// space complexity: O(1)
+
+var mostWordsFound = function (sentences) {
+    let maxWords = 0;
+    for (let i = 0; i < sentences.length; i++) {
+        let count = 1;
+        for (let j = 0; j < sentences[i].length; j++) {
+            if (sentences[i][j] === ' ') {
+                count++;
+            }
+        }
+        maxWords = Math.max(maxWords, count);
+    }
+    return maxWords;
+};
